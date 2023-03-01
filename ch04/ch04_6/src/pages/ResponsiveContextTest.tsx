346fb43b22737895f5c02f111e83150974482e34
Jongchan Ko <godol811@naver.com>
2023-03-01 23:59:20 +0900

    first commit

---
+++ ch04/ch04_6/src/pages/ResponsiveContextTest.tsx
@@ -0,0 +1,14 @@
+import {useResponsive} from "../contexts";
+import {Subtitle, Title} from "../components";
+
+export default function ResponsiveContextTest() {
+    const breakpoint = useResponsive()
+    return (
+        <section className={`mt-4`}>
+            <Title>ResponsiveContextTest</Title>
+            <div className={`mt-4`}>
+                <Subtitle> break point : {breakpoint}</Subtitle>
+            </div>
+        </section>
+    )
+}
---
+++ ch04/ch04_6/src/theme/daisyui/ValidatableInput.tsx
@@ -0,0 +1,36 @@
+import type {FC, DetailedHTMLProps, InputHTMLAttributes} from "react";
+import {forwardRef, useImperativeHandle, useMemo, useRef} from "react";
+import {ReactInputProps} from "./Input";
+
+export type ValidatableInputMethods = {
+    validate: () => [boolean,string]
+}
+
+export const ValidatableInput = forwardRef<ValidatableInputMethods, ReactInputProps>(({type,className: _className, ...inputProps}, methodsRef) => {
+
+    const className = useMemo(() => ['input',_className].join(' '),[_className])
+    const inputRef = useRef<HTMLInputElement>(null)
+
+    useImperativeHandle(
+        methodsRef,
+        () => ({
+            validate: (): [boolean,string] => {
+                const value = inputRef.current?.value
+                if (!value || !value.length) return [false, '사용자가 입력한 내용이 없습니다']
+
+                switch (type){
+                    case 'email':{
+                        const regEx = /^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/i
+                        const valid = regEx.test(value)
+                        return valid ? [true, value] : [false, '틀린 이메일 주소입니다.']
+                    }
+                }
+                return [true, value]
+            }
+        }),
+        [type]
+
+    )
+
+    return <input {...inputProps} className={className} ref={inputRef}/>
+})
